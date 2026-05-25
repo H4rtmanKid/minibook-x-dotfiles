52e365f52c4c553639cd914bb723dfd3d2e0744a
h4rtmankid <nattanondev@gmail.com>
2026-05-25 11:38:32 +0700

    feat: reset input to English on lock, run fastfetch on login

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Reset to English before locking (Fcitx5 doesn't work inside i3lock)
+fcitx5-remote -s keyboard-us 2>/dev/null  # switch Fcitx5 to English first
+fcitx5-remote -c 2>/dev/null              # then deactivate
+setxkbmap -layout us -option
+
 alpha='dd'
 selection='44475a'
 comment='6272a4'
@@ -9,7 +14,7 @@ red='ff5555'
 magenta='ff79c6'
 blue='6272a4'
 
-i3lock \
+XMODIFIERS='' i3lock \
   --insidever-color=$selection$alpha \
   --insidewrong-color=$selection$alpha \
   --inside-color=$selection$alpha \
@@ -47,3 +52,6 @@ i3lock \
   --verif-font="JetBrainsMono Nerd Font" \
   --wrong-font="JetBrainsMono Nerd Font" \
   --nofork
+
+# After unlock, restore dual-layout (--nofork blocks until unlocked)
+setxkbmap -layout us,th -option
